09fa3a67572198d846e0c46ef7c63a13a9c78c75
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2021-03-30 22:17:18 -0700

    wip(pages/api/hello): include Firebase SDK

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import * as admin from 'firebase-admin'
+
 export default (req, res) => {
+  const preventTreeShaking = admin
   res.status(200).json({ name: 'John Doe' })
 }
